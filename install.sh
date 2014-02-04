fccd91ced4f06045aac8bae36c6a04df322f6309
Daniel Earwicker <dan@earwicker.com>
2014-02-04 10:56:45 +0000

    Shell script wrappers

---
+++ install.sh
@@ -0,0 +1,2 @@
+ssh -p 9010 root@localhost vim-cmd vmsvc/snapshot.revert 11 32 0
+./client.js
---
+++ test.sh
@@ -0,0 +1,1 @@
+../TFS/trunk/UiTests/Smoke360/Smoke360/bin/Debug/Smoke360 http://10.0.0.110:9009/Prisym360
